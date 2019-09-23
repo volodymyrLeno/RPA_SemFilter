7b8fafa0b49ee57e8b5598e947fd6407bc516b79
stdevi <dvstas00@gmail.com>
2019-09-24 02:27:01 +0300

    Add multiple cases functionality Fix readLogsFromFile function

--- src/main/java/com/simplifier/Main.java
+++ src/main/java/com/simplifier/Main.java
@@ -5,94 +5,105 @@ import com.simplifier.rules.navigation.NavigationSimplifier;
 import com.simplifier.rules.read.ReadSimplifier;
 import com.simplifier.rules.write.WriteSimplifier;
 import com.simplifier.validation.Validator;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         String filePath = args[0];
         Utils utils = new Utils();
-        String logs = utils.readLogsFromFile(filePath);
+        Map<String, StringBuilder> cases = utils.readLogsFromFile(filePath);
 
-        /*
-         *  Validate log if there is action without id or name
-         */
-        Validator.validateForIdName(logs);
+        String writableLog = cases.values().stream().map(logCase -> {
+                    String logs = logCase.toString();
+                    try {
+                        Validator.validateForIdName(logs);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    String sortedLogs = PreProcessing.sortLog(logs);
+                    System.out.println("SORTED LOG\n");
+                    System.out.println(sortedLogs);
 
-        /*
-         *  Sort log before filtering
-         */
-        String sortedLogs = PreProcessing.sortLog(logs);
-        System.out.println("SORTED LOG\n");
-        System.out.println(sortedLogs);
+                    sortedLogs = PreProcessing.deleteChromeClipboardCopy(sortedLogs);
+                    sortedLogs = PreProcessing.mergeNavigationCellCopy(sortedLogs);
+                    sortedLogs = PreProcessing.deleteLink(sortedLogs);
+                    sortedLogs = PreProcessing.identifyPasteAction(sortedLogs);
+                    System.out.println("AFTER PRE PROCESSING\n");
+                    System.out.println(sortedLogs);
 
-        sortedLogs = PreProcessing.deleteChromeClipboardCopy(sortedLogs);
-        sortedLogs = PreProcessing.mergeNavigationCellCopy(sortedLogs);
-        sortedLogs = PreProcessing.deleteLink(sortedLogs);
-        sortedLogs = PreProcessing.identifyPasteAction(sortedLogs);
-        System.out.println("AFTER PRE PROCESSING\n");
-        System.out.println(sortedLogs);
+                    while (ReadSimplifier.isRedundantCopy(sortedLogs) ||
+                            ReadSimplifier.isSingleCopy(sortedLogs) ||
+                            NavigationSimplifier.isRedundantClickTextField(sortedLogs) ||
+                            WriteSimplifier.isRedundantChromePaste(sortedLogs) ||
+                            WriteSimplifier.isRedundantEditCell(sortedLogs) ||
+                            WriteSimplifier.isRedundantChromeEditField(sortedLogs) ||
+                            WriteSimplifier.isRedundantPasteIntoCell(sortedLogs) ||
+                            WriteSimplifier.isRedundantPasteIntoRange(sortedLogs)) {
 
-        // Add delete single copy
+                        if (ReadSimplifier.isRedundantCopy(sortedLogs)) {
+                            sortedLogs = ReadSimplifier.deleteRedundantCopy(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-        while (ReadSimplifier.isRedundantCopy(sortedLogs) ||
-                ReadSimplifier.isSingleCopy(sortedLogs) ||
-                NavigationSimplifier.isRedundantClickTextField(sortedLogs) ||
-                WriteSimplifier.isRedundantChromePaste(sortedLogs) ||
-                WriteSimplifier.isRedundantEditCell(sortedLogs) ||
-                WriteSimplifier.isRedundantChromeEditField(sortedLogs) ||
-                WriteSimplifier.isRedundantPasteIntoCell(sortedLogs) ||
-                WriteSimplifier.isRedundantPasteIntoRange(sortedLogs)) {
+                        if (ReadSimplifier.isSingleCopy(sortedLogs)) {
+                            sortedLogs = ReadSimplifier.deleteSingleCopy(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (ReadSimplifier.isRedundantCopy(sortedLogs)) {
-                sortedLogs = ReadSimplifier.deleteRedundantCopy(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (NavigationSimplifier.isRedundantClickTextField(sortedLogs)) {
+                            sortedLogs = NavigationSimplifier.deleteRedundantClickTextField(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (ReadSimplifier.isSingleCopy(sortedLogs)) {
-                sortedLogs = ReadSimplifier.deleteSingleCopy(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (WriteSimplifier.isRedundantChromePaste(sortedLogs)) {
+                            sortedLogs = WriteSimplifier.deleteRedundantChromePaste(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (NavigationSimplifier.isRedundantClickTextField(sortedLogs)) {
-                sortedLogs = NavigationSimplifier.deleteRedundantClickTextField(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (WriteSimplifier.isRedundantEditCell(sortedLogs)) {
+                            sortedLogs = WriteSimplifier.deleteRedundantEditCell(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (WriteSimplifier.isRedundantChromePaste(sortedLogs)) {
-                sortedLogs = WriteSimplifier.deleteRedundantChromePaste(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (WriteSimplifier.isRedundantChromeEditField(sortedLogs)) {
+                            sortedLogs = WriteSimplifier.deleteRedundantChromeEditField(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (WriteSimplifier.isRedundantEditCell(sortedLogs)) {
-                sortedLogs = WriteSimplifier.deleteRedundantEditCell(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (WriteSimplifier.isRedundantPasteIntoCell(sortedLogs)) {
+                            sortedLogs = WriteSimplifier.deleteRedundantPasteIntoCell(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
 
-            if (WriteSimplifier.isRedundantChromeEditField(sortedLogs)) {
-                sortedLogs = WriteSimplifier.deleteRedundantChromeEditField(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+                        if (WriteSimplifier.isRedundantPasteIntoRange(sortedLogs)) {
+                            sortedLogs = WriteSimplifier.deleteRedundantPasteIntoRange(sortedLogs);
+                            System.out.println("\n");
+                            System.out.println(sortedLogs);
+                        }
+                    }
+                    return sortedLogs;
+                }
+        ).collect(Collectors.joining()).replaceAll("\n\n", "");
 
-            if (WriteSimplifier.isRedundantPasteIntoCell(sortedLogs)) {
-                sortedLogs = WriteSimplifier.deleteRedundantPasteIntoCell(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
+        String[] sortedLog = writableLog.split("\\r?\\n");
+        Arrays.sort(sortedLog, Comparator.comparing(s -> s.substring(2)));
 
-            if (WriteSimplifier.isRedundantPasteIntoRange(sortedLogs)) {
-                sortedLogs = WriteSimplifier.deleteRedundantPasteIntoRange(sortedLogs);
-                System.out.println("\n");
-                System.out.println(sortedLogs);
-            }
-        }
+        System.out.println(StringUtils.join(Arrays.asList(sortedLog), "\n"));
 
         String newFilePath = filePath.substring(0, filePath.lastIndexOf(".")) + "_filtered.csv";
-        Utils.writeDataLineByLine(newFilePath, sortedLogs);
+        Utils.writeDataLineByLine(newFilePath, StringUtils.join(Arrays.asList(sortedLog), "\n"));
     }
 }
--- src/main/java/com/simplifier/Utils.java
+++ src/main/java/com/simplifier/Utils.java
@@ -1,11 +1,11 @@
 package com.simplifier;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.CSVWriter;
+import com.opencsv.*;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Utils {
 
@@ -27,7 +27,6 @@ public class Utils {
             String[] dataArray = data.split("\n");
             for (String row : dataArray) {
                 String[] cell = row.split(",");
-                System.out.println(cell[0].codePointAt(0));
                 writer.writeNext(cell);
             }
             writer.close();
@@ -36,18 +35,23 @@ public class Utils {
         }
     }
 
-    public String readLogsFromFile(String filePath) {
-        StringBuilder stringBuilder = new StringBuilder();
+    public Map<String, StringBuilder> readLogsFromFile(String filePath) {
+        Map<String, StringBuilder> cases = new HashMap<>();
+
         try {
+            RFC4180Parser rfc4180Parser = new RFC4180ParserBuilder().build();
             BufferedReader filereader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8));
             CSVReader csvReader = new CSVReaderBuilder(filereader)
                     .withSkipLines(1)
+                    .withCSVParser(rfc4180Parser)
                     .build();
 
             String[] nextLine;
             while ((nextLine = csvReader.readNext()) != null) {
-                System.out.println(nextLine[0].codePointAt(0));
-                stringBuilder.append(String.join(",", nextLine)).append("\n");
+                if (cases.get(nextLine[0]) == null) {
+                    cases.put(nextLine[0], new StringBuilder());
+                }
+                cases.get(nextLine[0]).append(String.join(",", nextLine)).append("\n");
             }
 
             csvReader.close();
@@ -56,6 +60,6 @@ public class Utils {
             e.printStackTrace();
         }
 
-        return stringBuilder.toString();
+        return cases;
     }
 }
